47091765e52499d61a59dd1505ab976237346998
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-09-08 11:11:14 +0200

    Add delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,14 @@ class ArticlesController < ApplicationController
     def create
       @article = Article.new(article_params)
       @article.save
-      
+
       redirect_to article_path(@article)
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+
+      redirect_to articles_path
     end
 end
