f222651ae8c67d8324eaf32180f9194dbc3e3281
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-09-09 10:14:03 +0200

    Add paperclip thumb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,2 @@
 class Tag < ApplicationRecord
-    has_many :taggings, :dependent => :delete_all 
-    has_many :articles, through: :taggings, :dependent => :delete_all 
-
-    def to_s
-        name
-    end
 end
